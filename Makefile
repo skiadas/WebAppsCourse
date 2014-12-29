7ea005224c6596080fc69b80e8c84aadbca2fc3f
Haris Skiadas <cskiadas@gmail.com>
2014-12-28 18:03:38 -0600

    Add make process

---
+++ Makefile
@@ -0,0 +1,17 @@
+TEMPLATE = template.html
+TEXTEMPLATE = template.tex
+MDFILES := $(shell find . -name \*.md)
+HTMLFILES := $(MDFILES:./%.md=site/%.html)
+# ASSIGNMENTS := $(filter ./assignments/%.md,$(MDFILES))
+PDFS := $(MDFILES:./%.md=site/%.pdf)
+# TEXS := $(ASSIGNMENTS:./%.md=site/%.tex)
+
+$(HTMLFILES): site/%.html: %.md $(TEMPLATE)
+	pandoc -o $@ --template=$(TEMPLATE) --mathjax --filter ./makeHtml.hs $<
+
+$(PDFS): site/%.pdf: %.md $(TEXTEMPLATE)
+	pandoc -o $@ --template=$(TEXTEMPLATE) -t latex --filter ./makeTex.hs $<
+
+site: $(HTMLFILES) $(PDFS)
+
+all: site
---
+++ makeHtml.hs
@@ -0,0 +1,36 @@
+import System.FilePath
+import Text.Pandoc.Definition
+import Text.Pandoc.Walk
+import Text.Pandoc.JSON
+
+changeLinkInline :: Inline -> Inline
+changeLinkInline (Str "ANSWERSPACE") = Space
+changeLinkInline (Link xs (t, n)) = Link xs (fixLink t, fixLink n)
+changeLinkInline x = x
+
+isMarkdown :: String -> Bool
+isMarkdown = (== ".md") . takeExtension
+
+fixLink :: String -> String
+fixLink s | isMarkdown s = replaceExtension s "html"
+          | otherwise    = s
+
+changeLinks :: Pandoc -> Pandoc
+changeLinks = walk changeLinkInline
+
+makeLink :: String -> Inline
+makeLink num   = Span attrs link
+   where attrs = ("", ["small"], [])
+         link  = [Space, Str "(", Link [Str "pdf"] (num ++ ".pdf", num), Str ")"]
+
+addLink :: [Inline] -> [Inline]
+addLink [Str num] = [Str num, makeLink num]
+addLink (x : xs) = x : addLink xs
+addLink [] = []
+
+makePDFLink :: Block -> Block
+makePDFLink (Header 1 attrs xs) = Header 1 attrs $ addLink xs
+makePDFLink x = x
+
+main :: IO ()
+main = toJSONFilter (changeLinks . (walk makePDFLink))
